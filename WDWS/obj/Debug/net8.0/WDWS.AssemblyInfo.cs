1c54adb59be7def0b85b51050904c44d64cb5493
dmilic1 <148439139+dmilic1@users.noreply.github.com>
2024-06-13 17:15:31 +0200

    login update

--- WDWS/obj/Debug/net8.0/WDWS.AssemblyInfo.cs
+++ WDWS/obj/Debug/net8.0/WDWS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WDWS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bf2cb1909e8edb616d71a9c985200e79cd30107")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f893f2e0202f77563cb103e6ae51d2f40bf9571")]
 [assembly: System.Reflection.AssemblyProductAttribute("WDWS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WDWS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
