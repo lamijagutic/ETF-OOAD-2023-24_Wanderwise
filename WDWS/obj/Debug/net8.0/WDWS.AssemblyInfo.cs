8cc73b9ac4effc07596ae1b66351a75ad3433e7d
Lamija Gutić <lgutic1@etf.unsa.ba>
2024-06-14 10:49:49 +0200

    Resetovana baza; nova migracija

--- WDWS/obj/Debug/net8.0/WDWS.AssemblyInfo.cs
+++ WDWS/obj/Debug/net8.0/WDWS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WDWS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a52be3c08ec73d32bbd632c32beeb86387876a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7f4928e4bb2b16cbbbea08cf378ff8bbffe3481")]
 [assembly: System.Reflection.AssemblyProductAttribute("WDWS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WDWS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
